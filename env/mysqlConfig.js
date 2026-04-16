d4bd9ee469bbd133298fc09db2d9c00006f4c31f
KingSadid <SadiSex07@gmail.com>
2026-04-16 13:41:05 -0500

    commits

--- env/mysqlConfig.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'test_db'
-};
-
-module.exports = config;
--- services/mysql.service.js
+++
@@ -1,6 +0,0 @@
-const mysql = require('mysql2/promise');
-const config = require('../env/mysqlConfig');
-
-const pool = mysql.createPool(config);
-
-module.exports = pool;
